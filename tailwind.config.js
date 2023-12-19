fe6757a419bc369d811fe741767e444ba45a86b0
Saikat Dutta <118626956+Saikatcoder@users.noreply.github.com>
2023-12-19 11:24:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Roboto":['Roboto', 'Arial','sans-serif']
+      },
+      spacing:{
+        '14' : '3.5rem',
+        '18': '4.5rem',
+      },
+    },
+  },
+  plugins: [],
+}
